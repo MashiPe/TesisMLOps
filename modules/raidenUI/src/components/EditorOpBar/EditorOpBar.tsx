6872905af91ad4f18334808faf30ed5306bb8cc0
MashiPe <penafieldavid471@gmail.com>
2023-01-02 23:18:59 -0500

    More changes to editor op bar

--- modules/raidenUI/src/components/EditorOpBar/EditorOpBar.tsx
+++ modules/raidenUI/src/components/EditorOpBar/EditorOpBar.tsx
@@ -1,36 +1,67 @@
-import { Button, Collapse, List, Menu, MenuProps } from 'antd'
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Button, Collapse, List, Menu, MenuProps, Popover } from 'antd'
 import Sider from 'antd/es/layout/Sider'
-import React from 'react'
+import React, { useState } from 'react'
 import CustomTrigger from '../CustomTrigger/CustomTrigger';
 import style from './EditorOpBar.module.scss' 
+// import type { MenuProps } from 'antd';
 
 type MenuItem = Required<MenuProps>['items'][number];
+type SelectEventHandler = Required<MenuProps>['onClick'];
 
 function getItem(
   label: React.ReactNode,
-  key: React.Key,
+  key?: React.Key | null,
   icon?: React.ReactNode,
   children?: MenuItem[],
+  type?: 'group',
 ): MenuItem {
   return {
     key,
     icon,
     children,
     label,
+    type,
   } as MenuItem;
 }
 
 const items: MenuItem[] = [
-  getItem('V1', '11'),
-  getItem('V2', '2'),
-  getItem('V3', '3'),
-  getItem('V4', '4'),
-  getItem('V5', '5'),
-  getItem('V6', '6'),
-  getItem('V7', '7'),
-  getItem('V8', '8'),
-];
-
+    getItem('Data Ingest', 'sub1', <MailOutlined />, [
+      getItem('Item 1', null, null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
+      getItem('Item 2', null, null, [getItem('Option 3', '3'), getItem('Option 4', '4')], 'group'),
+    ]),
+  
+    getItem('Transformation', 'sub2', <AppstoreOutlined />, [
+      getItem('Option 5', '5'),
+      getItem('Option 6', '6'),
+      getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
+    ]),
+  
+    getItem('Labeling', 'sub4', <SettingOutlined />, [
+      getItem('Option 9', '9'),
+      getItem('Option 10', '10'),
+      getItem('Option 11', '11'),
+      getItem('Option 12', '12'),
+    ]),
+    getItem('Tuning', 'sub5', <SettingOutlined />, [
+      getItem('Option 9', '19'),
+      getItem('Option 10', '110'),
+      getItem('Option 11', '111'),
+      getItem('Option 12', '112'),
+    ]),
+    getItem('Modeling', 'sub6', <SettingOutlined />, [
+      getItem('Option 9', '29'),
+      getItem('Option 10', '210'),
+      getItem('Option 11', '211'),
+      getItem('Option 12', '212'),
+    ]),
+    getItem('Validation', 'sub7', <SettingOutlined />, [
+      getItem('Option 9', '39'),
+      getItem('Option 10', '310'),
+      getItem('Option 11', '311'),
+      getItem('Option 12', '312'),
+    ])
+]
 const { Panel } = Collapse;
 
 const data = [
@@ -47,38 +78,52 @@ export interface EditorOpBarProps{
 }
 
 export default function EditorOpBar({collapsed, onCollapse}:EditorOpBarProps) {
-  return (
-    <div className={style.opbar}>
+
+    // const [selected,setSelected] = useState([""]);
+
+    // const handleSelect : SelectEventHandler = (e)=>{
+
+    //     console.log(e)
+
+    //     // setSelected(e.selectedKeys)
         
-            <Sider 
-                style={{height:'100%' , borderRadius:10}} 
-                theme='light' 
-                collapsible 
-                collapsed={collapsed} 
-                onCollapse={(value) => onCollapse(value)} 
-                width='300'
-                trigger={null}
-                >
-                    <div className={style.content}>
-
-                            <div className={style.main}>
-                                <Menu
-                                                // defaultSelectedKeys={['11']}
-                                                // defaultOpenKeys={['sub1']}
-                                                mode="inline"
-                                                theme="light"
-                                                items={items}
-                                                style={{border:0}}
-                                                selectedKeys={['11','2']}
-                                            /> 
-                            </div>
+    // }
 
 
-                        <CustomTrigger className={style.button} collpased={collapsed} onCollapse={()=>{onCollapse(!collapsed)}} />
+    return (
+        <div className={style.opbar}>
+            
+                <Sider 
+                    style={{height:'100%' , borderRadius:10}} 
+                    theme='light' 
+                    collapsible 
+                    collapsed={collapsed} 
+                    onCollapse={(value) => onCollapse(value)} 
+                    width='fit-content'
+                    trigger={null}
+                    >
+                        <div className={style.content}>
 
-                    </div>
-            </Sider>
+                                <div className={style.main}>
+                                    <Menu
+                                                    // defaultSelectedKeys={['11']}
+                                                    // defaultOpenKeys={['sub1']}
+                                                    mode="vertical"
+                                                    theme="light"
+                                                    items={items}
+                                                    style={{border:0, borderRadius:10}}
+                                                    // onClick={handleSelect}
+                                                    selectedKeys={['']}
+                                                    triggerSubMenuAction='click'
+                                                /> 
+                                </div>
 
-    </div> 
-  )
+
+                            <CustomTrigger className={style.button} collpased={collapsed} onCollapse={()=>{onCollapse(!collapsed)}} />
+
+                        </div>
+                </Sider>
+
+        </div> 
+    )
 }
